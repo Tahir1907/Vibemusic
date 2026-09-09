efd5627e721fc0092449f2a1619ee0cd78a0d24d
Tahir1907 <sus04569@gmail.com>
2026-09-09 14:19:15 +0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,40 +9,14 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "OuterTune"
+rootProject.name = "Vibemusic"
 include(":app")
 include(":innertube")
 include(":kugou")
 include(":lrclib")
 include(":material-color-utilities")
-include(":ffMetadataEx")
 include(":taglib")
 
-// Use a local copy of NewPipe Extractor by uncommenting the lines below.
-// We assume, that OuterTune and NewPipe Extractor have the same parent directory.
-// If this is not the case, please change the path in includeBuild().
-//
-// For this to work you also need to change the implementation in innertube/build.gradle.kts
-// to one which does not specify a version.
-// From:
-//      implementation(libs.newpipe.extractor)
-// To:
-//      implementation("com.github.teamnewpipe:NewPipeExtractor")
-
-//includeBuild("../NewPipeExtractor") {
-//    dependencySubstitution {
-//        substitute(module("com.github.teamnewpipe:NewPipeExtractor")).using(project(":extractor"))
-//    }
-//}
-
-
-includeBuild(file("media").toPath().toRealPath().toAbsolutePath().toString()) {
-    dependencySubstitution {
-        substitute(module("androidx.media3:media3-common")).using(project(":lib-common"))
-        substitute(module("androidx.media3:media3-common-ktx")).using(project(":lib-common-ktx"))
-        substitute(module("androidx.media3:media3-datasource-okhttp")).using(project(":lib-datasource-okhttp"))
-        substitute(module("androidx.media3:media3-exoplayer")).using(project(":lib-exoplayer"))
-        substitute(module("androidx.media3:media3-exoplayer-workmanager")).using(project(":lib-exoplayer-workmanager"))
-        substitute(module("androidx.media3:media3-session")).using(project(":lib-session"))
-    }
+if (file("ffMetadataEx").exists()) {
+    include(":ffMetadataEx")
 }
