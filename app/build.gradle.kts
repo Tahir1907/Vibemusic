451886c60e3b40f2018dc0285fe5175381d8a7f0
Tahir1907 <sus04569@gmail.com>
2026-09-09 09:55:21 +0400

    Fix typo in task exclusion for NextRenderersFactory

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -134,7 +134,7 @@ android {
     tasks.withType<KotlinCompile> {
         if (!name.substringAfter("compile").lowercase().startsWith("full")) {
             exclude("**/*FFmpegScanner.kt")
-            exclude("**/*NextRendersFactory.kt")
+            exclude("**/*NextRenderersFactory.kt")
         } else {
             exclude("**/*FFmpegScannerDud.kt")
             exclude("**/*ffdecoderDud.kt")
