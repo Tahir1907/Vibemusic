66f76a56decd357391930d2c06a11eed7b25884d
GitHub Action <action@github.com>
2026-09-09 09:37:04 +0000

    Sync code from OuterTune dev

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,17 +9,14 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Vibemusic"
+rootProject.name = "OuterTune"
 include(":app")
 include(":innertube")
 include(":kugou")
 include(":lrclib")
 include(":material-color-utilities")
+include(":ffMetadataEx")
 include(":taglib")
-
-if (file("ffMetadataEx").exists()) {
-    include(":ffMetadataEx")
-}
 
 // Use a local copy of NewPipe Extractor by uncommenting the lines below.
 // We assume, that OuterTune and NewPipe Extractor have the same parent directory.
@@ -37,3 +34,15 @@ if (file("ffMetadataEx").exists()) {
 //        substitute(module("com.github.teamnewpipe:NewPipeExtractor")).using(project(":extractor"))
 //    }
 //}
+
+
+includeBuild(file("media").toPath().toRealPath().toAbsolutePath().toString()) {
+    dependencySubstitution {
+        substitute(module("androidx.media3:media3-common")).using(project(":lib-common"))
+        substitute(module("androidx.media3:media3-common-ktx")).using(project(":lib-common-ktx"))
+        substitute(module("androidx.media3:media3-datasource-okhttp")).using(project(":lib-datasource-okhttp"))
+        substitute(module("androidx.media3:media3-exoplayer")).using(project(":lib-exoplayer"))
+        substitute(module("androidx.media3:media3-exoplayer-workmanager")).using(project(":lib-exoplayer-workmanager"))
+        substitute(module("androidx.media3:media3-session")).using(project(":lib-session"))
+    }
+}
